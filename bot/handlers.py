ae7a43ae7ca970002216a35deca860c04e1a889c
Samer <samershams26@gmail.com>
2025-05-11 21:22:36 +0500

    изменил импорт также изменил dockerfile

--- bot/handlers.py
+++ bot/handlers.py
@@ -9,7 +9,7 @@ from aiogram.fsm.state import State
 from aiogram.types import ReplyKeyboardMarkup
 from aiogram.types import KeyboardButton
 
-from services.telegram_review_service import download_telegram_file
+from app.services.telegram_review_service import download_telegram_file
 
 from states import ReviewStates
 
